ae1ef46000161d09280bdd0f78c5f8efa4e59438
zhuzhe <zhuzhe@findpiano.cn>
2019-03-12 22:38:18 +0800

    优化page config

--- h5/vue.config.js
+++ h5/vue.config.js
@@ -1,24 +1,15 @@
-const {
-  autoPages
-} = require('./build/page.config')
 const path = require('path')
 const resolve = (dir) => {
   return path.join(__dirname, dir)
 }
 const isDev = process.env.NODE_ENV === 'development'
 
-const pages = {
-  ...autoPages(['index']), // 首页
-  ...autoPages(['helloh5']), // helloh5 模块页面
-  ...autoPages(['hellomui']) // hellomui 模块页面
-}
-
-const PackerAppPlugin = require('./build/plugins/packapp.plugin')
+const { PackerAppPlugin } = require('./build/plugins')
 
 module.exports = {
   publicPath: isDev ? '/' : './',
   productionSourceMap: isDev,
-  pages,
+  pages: require('./pages.config'),
   chainWebpack: (config) => {
     config.resolve.alias
       .set('~', resolve('src'))
